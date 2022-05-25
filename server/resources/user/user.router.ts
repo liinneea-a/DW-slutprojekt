689ac46ea2336326ba84348174044bfc474a1fc7
linnea <linnea.albertsson@gmail.com>
2022-05-25 11:41:34 +0200

    Authorize works

--- server/resources/user/user.router.ts
+++ server/resources/user/user.router.ts
@@ -1,20 +1,26 @@
 import express from "express";
-import { addUser, deleteUser, getAllUsers, getLoggedInUser, getUser, loginUser, logoutUser, updateUser } from "./user.controller";
+import { getAllUsers } from "./user.controller";
+import { authorize } from "./user.middlewares";
+
+
 
 export const userRouter = express
   .Router()
-  // .get("/user", ifAdmin, getAllUsers)
+  .get("/users", authorize, getAllUsers)
+  //.get('/loggedin', getLoggedInUser)
   // .get("/user/:id", ifAdmin, ifAdminOrSelf, getUser)
-  // .post("/user", validate, addUser)
+  // .post("/user", addUser)
+  // .post('/login', loginUser)
   // .put("/user/:id", authorize, ifAdminOrSelf, updateUser)
-  // .delete("/user/:id", authorize, ifAdminOrSelf, deleteUser);
+  // .delete("/user/:id", authorize, ifAdminOrSelf, deleteUser)
+  // .delete('/logout', authorize, logoutUser);
   
-  // TODO: lägg till säkerhet
-  .get("/users", getAllUsers)
-  .get("/user/:id", getUser)
-  .post("/user", addUser)
-  .put("/user/:id", updateUser)
-  .delete("/user/:id", deleteUser)
-  .post('/login', loginUser)
-  .get('/loggedin', getLoggedInUser)
-  .delete('/logout', logoutUser)
+  //TODO: lägg till säkerhet
+  // .get("/users", getAllUsers)
+  // .get("/user/:id", getUser)
+  // .post("/user", addUser)
+  // .put("/user/:id", updateUser)
+  // .delete("/user/:id", deleteUser)
+  // .post('/login', loginUser)
+  // .get('/loggedin', getLoggedInUser)
+  // .delete('/logout', logoutUser)
