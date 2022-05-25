c6b756ff0e2cebce0647870cddb33a6d7dd4dfb2
linnea <linnea.albertsson@gmail.com>
2022-05-25 11:41:03 +0200

    Deleted middleware.ts and moved functions to user.middleware

--- server/resources/user/user.middlewares.ts
+++ server/resources/user/user.middlewares.ts
@@ -1,2 +1,33 @@
-import { Request, Response } from "express";
-import { HttpError } from "../../error";
+import { NextFunction, Request, Response } from "express";
+import { UserModel } from "./user.model";
+
+/** Stops users that aren't logged in */
+export const authorize = (req: Request, res: Response, next: NextFunction) => {
+  
+    if (req.session?.isPopulated) {
+      next();
+    } else {
+     return res.status(401).json("You must login");
+    }
+  };
+  
+  export const ifAdmin = (req: Request, res: Response, next: NextFunction) => {
+   
+    if (req.session?.user?.isAdmin) {
+      console.log(req.session);
+      next();
+    } else {
+     return res.status(403).json("in ifAdmin. You don't have the rights to do this...");
+    }
+  };
+  
+  export const ifAdminOrSelf = async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserModel.findById(req.params.id);
+    const isAdmin = req.session?.user.isAdmin;
+    const myContent = user?.id && user === req.session?.user.id 
+  
+    if(!isAdmin && !myContent) {
+      res.status(403).json('forbidden')
+    }
+  next();
+  }
